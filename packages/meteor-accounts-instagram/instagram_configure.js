db106aac59735aacfe3161beaf6e85152e08c359
Tim Brandin <tim.brandin@gmail.com>
2013-05-09 11:26:04 +0200

    Added bower package managing.

---
+++ packages/meteor-accounts-instagram/instagram_configure.js
@@ -0,0 +1,11 @@
+Template.configureLoginServiceDialogForInstagram.siteUrl = function () {
+    return Meteor.absoluteUrl({replaceLocalhost: true});
+};
+
+Template.configureLoginServiceDialogForInstagram.fields = function () {
+    return [
+        {property: 'clientId', label: 'Client Id'},
+        {property: 'secret', label: 'Client Secret'},
+        {property: 'scope', label: "Scope (separated by a '+')"}
+    ];
+};
