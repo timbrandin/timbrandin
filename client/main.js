6dabac6f582920f9e7020988dccb568c8089ed19
timbrandin <tim.brandin@wunderkraut.com>
2013-05-17 15:43:52 +0200

    Added import of LinkedIn data.

--- client/main.js
+++ client/main.js
@@ -11,29 +11,62 @@ Template.main.rendered = function() {
     'max-height': $(window).height(),
     'background': 'hsl(' + hue + ', 70%, 70%)'
   });
-  $(window).load(function() {
-    $('.gallery').flexslider({
-      animation: 'slide',
-      slideshow: true,
-      slideshowSpeed: 4000 + 3000 * Math.random(),
-      animationSpeed: 600,
-      controlNav: false,
-      directionNav: false
-    });
-  });
+//  $(window).load(function() {
+//    $('.gallery').flexslider({
+//      animation: 'slide',
+//      slideshow: true,
+//      slideshowSpeed: 4000 + 3000 * Math.random(),
+//      animationSpeed: 600,
+//      controlNav: false,
+//      directionNav: false
+//    });
+//  });
 };
 
-Template.main.importedProfile = function() {
-  return Profile.findOne({});
-};
-
-Template.main.name = function () {
-  return 'Name Lastname';
-};
-
-Template.main.events({
-  'click .import a' : function (e) {
-    e.preventDefault();
-    console.log('import me');
+// Create a profile from the first logged in user.
+Deps.autorun(function(){
+  if(Meteor.userId() && !Meteor.loggingIn()){
+    Meteor.call('createProfile');
   }
 });
+
+// If we're using Handlebars, register the {{importedProfile}} and
+// some more global helpers.
+if (typeof Handlebars !== 'undefined') {
+  Handlebars.registerHelper('importedProfile', function () {
+    return Profile.findOne({});
+  });
+  Handlebars.registerHelper('dateFormat', function (date) {
+    return (date.month < 10 ? '0' + date.month : date.month) + '/' + date.year;
+  });
+  Handlebars.registerHelper('suffix', function (obj, suffix) {
+    return obj ? obj + suffix : '';
+  });
+  Handlebars.registerHelper('dateLength', function (start, end) {
+    var d1 = new Date(start.year, start.month - 1),
+        d2 = !end.month ? Date.now() : new Date(end.year, end.month - 1),
+        diff = (d2 - d1) / 1000,
+        day_ago = Math.floor(diff / 86400);
+
+    if ( isNaN(day_ago) || day_ago < 0) {
+      return null;
+    }
+
+    return day_ago == 0 ? (
+        diff < 60 ? "just now" : (
+        diff < 120 ? "1 minute ago" : (
+        diff < 3600 ? Math.floor( diff / 60 ) + " minutes ago" : (
+        diff < 7200 ? "1 hour ago" : (
+        diff < 86400 ? Math.floor( diff / 3600 ) + " hours ago"
+      : ''))))) : (
+      day_ago == 1 ? "Yesterday" : (
+      day_ago < 7 ? day_ago + " days" : (
+      day_ago < 31 ? Math.ceil( day_ago / 7 ) + " weeks" : (
+      day_ago < 365 ? Math.ceil( day_ago / 30.5 ) + " months" : (
+      Math.floor( day_ago / 365.25 ) + " years "
+        + Math.ceil( (day_ago - Math.floor( day_ago / 365.25 ) * 365.25) / 30.5 )
+        + " months"
+    )))));
+
+  });
+}
--- server/model.js
+++ server/model.js
@@ -1,2 +1,38 @@
 
 Profile = new Meteor.Collection("profile");
+
+Meteor.methods({
+  createProfile: function(options) {
+    var user = Meteor.user();
+    if (user && Profile.find({}).count() == 0) {
+      if (user.services.linkedin) {
+        var service = user.services.linkedin;
+        var profile = getProfile(service.id, service.accessToken);
+        Profile.insert(profile);
+      }
+    }
+    else {
+      if (user.services.linkedin) {
+        var service = user.services.linkedin;
+        var profile = getProfile(service.id, service.accessToken);
+        Profile.update({}, {$set: profile});
+      }
+    }
+  }
+});
+
+var getProfile = function (id, accessToken) {
+  var result = Meteor.http.get(
+    "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,headline,picture-url,summary,associations,positions,specialties,honors,interests,skills,educations,courses,recommendations-received,phone-numbers)", {
+      params: {
+        oauth2_access_token: accessToken,
+        format: 'json'
+      }
+    });
+  if (result.error) {
+    throw new Error("Failed to fetch profile from LinkedIn. " +
+      "HTTP Error " + result.statusCode + ": " + result.content);
+  } else {
+    return result.data;
+  }
+};
